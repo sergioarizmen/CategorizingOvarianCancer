fde1ce512531850f1839bba508f2461fe98d10bc
SergioAFRTM <sergioarizmen@gmail.com>
2023-12-11 05:59:00 -0600

    fix: corrected bug in image preprocessor
    Updated 'image_preprocessor.py' to fix dimension error, where grayscaling removes the third dimension in the image arrays, but this change was not propagated to the '_get_patches' method.

--- image_preprocessor.py
+++ image_preprocessor.py
@@ -1,7 +1,10 @@
-import numpy as np
 import cv2 as cv
+import numpy as np
+
+from default.exception_decorator import log_exception
 
 
+@log_exception
 def _get_patches(image,
                  output_patch_width: int = 250,
                  output_patch_height: int = 250,
@@ -20,17 +23,17 @@ def _get_patches(image,
     height, width = image.shape[:2]
 
     # Initialize an empty array of patches.
-    patches = np.array([], dtype=image.dtype)
+    patches = []
 
     # Loop through the image with a step size of 250 pixels.
-    for y in range(0, height, 250):
-        for x in range(0, width, 250):
+    for y in range(0, height, output_patch_height):
+        for x in range(0, width, output_patch_width):
             # Get the height and width of the current patch
-            patch_height = min(250, height - y)
-            patch_width = min(250, width - x)
+            patch_height = min(output_patch_height, height - y)
+            patch_width = min(output_patch_width, width - x)
 
             # Crop a h by 250 pixel patch from the image.
-            patch = image[y:y + patch_height, x:x + patch_width]
+            patch = image[y:y + patch_height, x:x + patch_width, ...]
 
             # Calculate the mean value of the patch pixels
             mean = np.mean(patch)
@@ -44,24 +47,25 @@ def _get_patches(image,
             if patch_height < output_patch_height or patch_width < output_patch_width:
                 # Pad the image with random noise.
                 # Generate random noise with the same shape and type as the patch.
-                noise = np.random.randint(0, 256, (output_patch_height, output_patch_width, 3), dtype=patch.dtype)
+                noise = np.random.randint(0, 256, (output_patch_height, output_patch_width), dtype=patch.dtype)
 
                 # Replace a patch sized space in the noise with zeroes.
-                noise[0:patch_height, 0:patch_width, :] = patch
+                noise[0:patch_height, 0:patch_width, ...] = patch
 
                 # Replace the patch with the noise filled patch.
                 patch = noise
 
             # Add the patch to the array of patches.
-            np.append(patches, patch)
+            patches.append(patch)
 
     # Return the patches.
-    return patches
+    return np.array(patches, dtype=image.dtype)
 
 
+@log_exception
 def preprocess_image(image):
     """
-    Convert to grayscale and pass into patches.
+    Convert to grayscale and pass into patches of type 'float32'.
     :param image: The image to preprocess.
     :return: The preprocessed patches.
     """
@@ -71,6 +75,9 @@ def preprocess_image(image):
 
     # Get the patched up image.
     image = _get_patches(image)
+
+    # Transform the number type.
+    image = image.astype('float32')
 
     # Return the preprocessed image.
     return image
